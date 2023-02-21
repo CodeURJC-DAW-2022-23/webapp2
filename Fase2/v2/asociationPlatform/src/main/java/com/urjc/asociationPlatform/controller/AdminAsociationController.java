e1b6abd00afa4c5474e6d7e536f4f1a9f0c2a97b
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-02-21 19:47:17 +0100

    Lista/table añadida

--- Fase2/v2/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/AdminAsociationController.java
+++ Fase2/v2/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/AdminAsociationController.java
@@ -1,11 +1,13 @@
 package com.urjc.asociationPlatform.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.urjc.asociationPlatform.model.Asociation;
 import com.urjc.asociationPlatform.service.AsociationService;
@@ -16,25 +18,25 @@ public class AdminAsociationController {
     @Autowired
     AsociationService asoService;
 
-    @GetMapping("/editAsoc")
+    @GetMapping("/adminAsoc")
     public String listAsociations(Model model){
         List<Asociation> list = asoService.findAll();
         model.addAttribute("asocList", list);
         return "asociations"; 
     }
 
-    /*
-    @GetMapping("/editarUsuarios/{id}")
-	public String obtainUser(Model model, @PathVariable long id) {
+    
+    @GetMapping("/editAsoc/{id}")
+	public String obtainAsoc(Model model, @PathVariable long id) {
 
-		User user = userService.findById(id);
+		Optional<Asociation> asoc = asoService.findById(id);
 
-		model.addAttribute("user", user);
+		model.addAttribute("asociation", asoc);
 
 		return "";
 	}
 
-
+/* 
     @GetMapping("/editarUsuario/{id}")
 	public String obtainUser(Model model, @PathVariable long id) {
 		User user = userService.findById(id);
