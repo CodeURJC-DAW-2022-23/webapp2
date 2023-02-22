9d5df9716a39688061700963827880881a3d5f04
DanielGallardo00 <59607988+DanielGallardo00@users.noreply.github.com>
2023-02-22 17:52:59 +0100

    modificar completado

--- Fase2/v2Andrea/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/UserController.java
+++ Fase2/v2Andrea/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/UserController.java
@@ -43,14 +43,15 @@ public class UserController {
 	@PostMapping("/editarUsuarios/{id}")
 	public String editProfile(Model model, User newUser,@PathVariable long id){ 
 		try { User user = userService.findById(id).orElseThrow();
-			// System.out.println("User in Database: " + user.getUsername());
-			// System.out.println("New User: " + newUser.getUsername());
+			
 			user.setUsername(newUser.getUsername());
+			user.setEmail(newUser.getEmail());
+			user.setRol(newUser.getRol());
 			userService.save(user);
 
-			// System.out.println("User after save in database: " + user.getUsername());
-			return "redirect:/home";
+			return "redirect:/editarUsuarios";
 		} catch (Exception e) {
+			e.printStackTrace();
 			return "redirect:/error"; 
 		}
 	}
