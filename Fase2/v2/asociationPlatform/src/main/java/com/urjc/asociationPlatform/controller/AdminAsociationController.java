2a18b9424136439fd0362fe69ffba2d7cce59fc3
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-02-22 19:54:26 +0100

    maquetado

--- Fase2/v2/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/AdminAsociationController.java
+++ Fase2/v2/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/AdminAsociationController.java
@@ -1,5 +1,7 @@
 package com.urjc.asociationPlatform.controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.urjc.asociationPlatform.model.Asociation;
 import com.urjc.asociationPlatform.service.AsociationService;
@@ -20,24 +23,34 @@ public class AdminAsociationController {
 
     @GetMapping("/adminAsoc")
     public String listAsociations(Model model){
+
         List<Asociation> list = asoService.findAll();
         model.addAttribute("asocList", list);
         return "asociations"; 
+
     }
     
     @GetMapping("/editAsociations/{id}")
 	public String obtainAsoc(Model model, @PathVariable long id) {
-
+		/* 
 		Optional<Asociation> asoc = asoService.findById(id);
-
-		model.addAttribute("asociation", asoc);
-
-		return "editAsociations";
+		if(asoc.isPresent() == false)
+			return "404";
+		else{
+			model.addAttribute("asociation", asoc);
+			return "editAsociations";
+		}*/
+		try { Asociation asoc = asoService.findById(id).orElseThrow();
+			model.addAttribute("asociation", asoc);
+			return "editAsociations";
+        } catch (Exception e) {
+            return "404";
+        }
 	}
 
-/* 
-	@PostMapping("/editarUsuario")
-	public String editProfile(Model model, User newUser) throws IOException, SQLException {
+	/* 
+	@PostMapping("/editAsociations")
+	public String editProfile(Model model, Asociation asoc) throws IOException, SQLException {
 		userService.setUsername(newUser.getUsername());
 		newUser.getEncodedPassword();
 		if (!newUser.getEncodedPassword().equals("")){
