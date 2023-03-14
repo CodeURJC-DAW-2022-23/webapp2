5c791c13b2ba4a8dcce8cfdb6a6deb5c003fb357
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-03-14 20:40:55 +0100

    Update import.sql

--- Fase2/import.sql
+++ Fase2/import.sql
@@ -1,6 +1,7 @@
 INSERT INTO `user` VALUES (1,NULL,'admin@gmail.com','$2a$10$yMrEp.aQS7jPr.m6dTPSge/i5c0Tk.jFflmWYggFsUuhIRjCYFZT6','ADMIN','admin',TRUE);
 INSERT INTO `user` VALUES (2,NULL,'user1@gmail.com','$2a$10$2qBZq0zbzRwQsJwAd8dqsOv2kICns.l/7DUWdMhtZ2anxTx/3bPzO','BASE','user',TRUE);
 INSERT INTO `user` VALUES (3,NULL,'asociation@gmail.com','$2a$10$fFOE0amCpdIskxvCG8g7kuU0FmAdID8m7CvpWGB/4Du95ciM4mP5G','ASO','aso',TRUE);
-INSERT INTO `asociation` VALUES (1,'Móstoles','Hola xD','Facultad de Ciencias de la Comunicación','Roleros en linea',3),(2,'Fuenlabrada','Desc','Facultad de Ciencias Jurídicas y Políticas','Anger',NULL);
+INSERT INTO `asociation` VALUES (1,'Móstoles','Hola xD','Facultad de Ciencias de la Comunicación','Roleros en linea',3);
+INSERT INTO `asociation` VALUES (2,'Fuenlabrada','Desc','Facultad de Ciencias Jurídicas y Políticas','Anger',NULL);
 INSERT INTO `event` VALUES (1,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test','12:00',1);
 INSERT INTO `event` VALUES (2,TRUE,'MÓSTOLES',TRUE,'2023-04-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test2','12:00',2);
