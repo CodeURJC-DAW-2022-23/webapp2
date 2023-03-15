d7322e08d00ca7b5d535f6cfb2d18cf4bb6ea3ba
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-03-15 19:22:33 +0100

    Update import.sql

--- Fase2/import.sql
+++ Fase2/import.sql
@@ -4,4 +4,11 @@ INSERT INTO `user` VALUES (3,NULL,'asociation@gmail.com','$2a$10$fFOE0amCpdIskxv
 INSERT INTO `asociation` VALUES (1,'Móstoles','Hola xD','Facultad de Ciencias de la Comunicación','Roleros en linea',3);
 INSERT INTO `asociation` VALUES (2,'Fuenlabrada','Desc','Facultad de Ciencias Jurídicas y Políticas','Anger',NULL);
 INSERT INTO `event` VALUES (1,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test','12:00',1);
+
 INSERT INTO `event` VALUES (2,TRUE,'MÓSTOLES',TRUE,'2023-04-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test2','12:00',2);
+INSERT INTO `event` VALUES (3,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test3','12:00',1);
+INSERT INTO `event` VALUES (4,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test4','12:00',1);
+INSERT INTO `event` VALUES (5,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test5','12:00',1);
+INSERT INTO `event` VALUES (6,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test6','12:00',1);
+INSERT INTO `event` VALUES (7,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test7','12:00',1);
+INSERT INTO `event` VALUES (8,TRUE,'MÓSTOLES',TRUE,'2023-03-25','test','6h 0min','18:00',NULL,'Aul','MARZO','Test8','12:00',1);
