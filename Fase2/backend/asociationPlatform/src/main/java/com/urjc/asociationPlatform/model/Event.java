7d365d682589d14a4913e0420e2a7cbd41d50f2a
RudberGames <rudbergames@gmail.com>
2023-03-18 12:03:30 +0100

    Add equals and hashCode for asociation, comment, event and user

--- Fase2/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/model/Event.java
+++ Fase2/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/model/Event.java
@@ -19,6 +19,29 @@ import java.util.Set;
 
 @Entity
 public class Event {
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Event other = (Event) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="Id")
