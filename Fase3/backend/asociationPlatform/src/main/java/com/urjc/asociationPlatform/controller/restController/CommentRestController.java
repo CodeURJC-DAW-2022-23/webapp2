84beba2db7be4fc1000d96d713c6e4fff4a36c28
AndreaCardenalMillan <103501965+AndreaCardenalMillan@users.noreply.github.com>
2023-03-20 18:24:55 +0100

    imports

--- Fase3/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/restController/CommentRestController.java
+++ Fase3/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/restController/CommentRestController.java
@@ -1,22 +1,15 @@
 package main.java.com.urjc.asociationPlatform.controller.restController;
 
-import java.security.Principal;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import javax.servlet.http.HttpServletRequest;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.urjc.asociationPlatform.model.Comment;
-import com.urjc.asociationPlatform.model.Event;
 import com.urjc.asociationPlatform.service.CommentService;
-import com.urjc.asociationPlatform.service.EventService;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -25,7 +18,7 @@ public class CommentRestController {
     private CommentService commentService;
     
     @GetMapping("/{id}")
-    public ResponseEntity<Review> getComment(@PathVariable long id) {
+    public ResponseEntity<Comment> getComment(@PathVariable long id) {
         
         Optional<Comment> comment = commentService.findById(id);
         if (comment.isPresent()) {
@@ -34,4 +27,7 @@ public class CommentRestController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    //añadir comentario
+    //añadir likes
 }
