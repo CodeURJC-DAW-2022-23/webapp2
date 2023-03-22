a66f564ecbea5caaf526f6085e1c177de6640a9e
Fiorella <fiorellavictoria70@gmail.com>
2023-03-22 15:36:39 +0100

    add AssoRestController methods

--- Fase2/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/restController/AssoRestController.java
+++ Fase2/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/restController/AssoRestController.java
@@ -1,12 +1,15 @@
 package com.urjc.asociationPlatform.controller.restController;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,32 +48,80 @@ public class AssoRestController {
 
   @GetMapping("/miAsociacion")
   public ResponseEntity<Asociation> getMyAsso(HttpServletRequest request) {
-    User currentUser = getUser(request);
-    if (currentUser == null){
-      System.out.println("1");
-      return ResponseEntity.notFound().build();
-    }
-      
-    Optional<Asociation> aso = assoService.findByOwner(currentUser);
-    if (aso.isEmpty()){
+    User currentUser = checkAdminOrAsso("ASO", request);
+    
+    if (currentUser == null)
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    Optional<Asociation> asso = assoService.findByOwner(currentUser);
+    if (asso.isEmpty()) {
       System.out.println("2");
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Asociation> getById(@PathVariable long id) {
+    Optional<Asociation> asso = assoService.findById(id);
+    if (asso.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @GetMapping("/asociationsList")
+  public ResponseEntity<List<Asociation>> ListAsso() {
+    return ResponseEntity.ok(assoService.findAll());
+  }
+
+  @PutMapping("/miAsociacion")
+  public ResponseEntity<Asociation> editMyAsso(@RequestBody Asociation asso, HttpServletRequest request) {
+    return edditAssoAux("ASO", request, asso);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Asociation> editAsso(@RequestBody Asociation asso, HttpServletRequest request) {
+    return edditAssoAux("ADMIN", request, asso);
+  }
+  
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Asociation> deleteAsso(@PathVariable long id, HttpServletRequest request) {
+    if (checkAdminOrAsso("ADMIN", request) == null)
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+
+    Optional<Asociation> asso = assoService.findById(id);
+    if (asso.isEmpty()) {
       return ResponseEntity.notFound().build();
     }
 
-    return ResponseEntity.ok(aso.get()); //🥹
+    assoService.deleteById(id);
+    return ResponseEntity.ok(asso.get());
   }
 
-  @PutMapping("miAsociacion/{id}")
-  public ResponseEntity<Asociation> putMethodName(@PathVariable Long id, @RequestBody Asociation asso) {
-      Optional<Asociation> oldAso = assoService.findById(id);
+  private ResponseEntity<Asociation> edditAssoAux(String type, HttpServletRequest request, @RequestBody Asociation asso) {
+    User currentUser = checkAdminOrAsso(type, request);
+    
+    if (currentUser == null)
+      return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
-      if (oldAso.isEmpty())
-        return ResponseEntity.notFound().build();
+    Optional<Asociation> oldAsso = assoService.findById(currentUser.getId());
 
-      asso.setId(id);
-      assoService.save(asso);
-      return ResponseEntity.ok(asso);
+    if (oldAsso.isEmpty())
+      return ResponseEntity.notFound().build();
+
+    asso.setId(currentUser.getId());
+    assoService.save(asso);
+    return ResponseEntity.ok(asso);
   }
-  
-  
+
+  private User checkAdminOrAsso(String type, HttpServletRequest request) {
+    User currentUser = getUser(request);
+    if (currentUser == null || !currentUser.getRol().equals(type))
+      return null;
+
+    return currentUser;
+  }
 }
