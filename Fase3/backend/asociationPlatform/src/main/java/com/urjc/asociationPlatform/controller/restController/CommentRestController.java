090fc6f9405296554197f763870cb6cc5a21bec9
AndreaCardenalMillan <103501965+AndreaCardenalMillan@users.noreply.github.com>
2023-03-25 12:09:04 +0100

    metodos añadir y borrar comentarios

--- Fase3/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/restController/CommentRestController.java
+++ Fase3/backend/asociationPlatform/src/main/java/com/urjc/asociationPlatform/controller/restController/CommentRestController.java
@@ -1,16 +1,27 @@
 package com.urjc.asociationPlatform.controller.restController;
 
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.urjc.asociationPlatform.model.Comment;
 import com.urjc.asociationPlatform.service.CommentService;
-
+import com.urjc.asociationPlatform.model.Event;
+import com.urjc.asociationPlatform.service.EventService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -23,6 +34,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 public class CommentRestController {
     @Autowired
     private CommentService commentService;
+
+    @Autowired
+    private EventService eventService;
+    
     
     @Operation(summary = "Get Comment")
     @ApiResponses(value = {
@@ -46,6 +61,61 @@ public class CommentRestController {
         }
     }
 
-    //añadir comentario
-    //borrar
+    @Operation(summary = "Post Comment")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "comment posted sucessfully",content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Comment.class))}),
+        @ApiResponse(responseCode = "400", description = "invalid id supplied", content = @Content),
+        @ApiResponse(responseCode = "401", description = "comment is not created", content = @Content),
+        @ApiResponse(responseCode = "403", description = "not enough privileges or admin is modifying itself", content = @Content),
+        @ApiResponse(responseCode = "404", description = "comment not found", content = @Content)
+            
+    })
+
+    @PostMapping("/crearComentario/{id}")
+	public ResponseEntity<Comment> postComment(Model model, @RequestParam Comment newComent, @PathVariable long id, HttpServletRequest request){
+
+        Principal principal = request.getUserPrincipal();
+
+        if(principal != null){
+            try {
+                String fecha = DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm:ss a").format(LocalDateTime.now());
+                newComent.setTime(fecha.toString());
+                newComent.setCommentUser(principal.getName());
+                commentService.save(newComent);
+            
+                Event event = eventService.findById(id).orElseThrow();
+                event.addComment(newComent);
+                eventService.save(event);
+
+                return new ResponseEntity<>(HttpStatus.OK);
+            } catch (NoSuchElementException e) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } 
+	}
+
+    @Operation(summary = "Remove Comment")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "comment removed sucessfully",content = {
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Comment.class))}),
+        @ApiResponse(responseCode = "400", description = "invalid id supplied", content = @Content),
+        @ApiResponse(responseCode = "401", description = "comment is not created", content = @Content),
+        @ApiResponse(responseCode = "403", description = "not enough privileges or admin is modifying itself", content = @Content),
+        @ApiResponse(responseCode = "404", description = "comment not found", content = @Content)
+            
+    })
+
+    @PostMapping("/admin/editarComentarios/{id}/delete")
+	public ResponseEntity<Comment> deleteComment(Model model,@RequestParam Comment newComent, @PathVariable long id){
+        try {
+            commentService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+	}
 }
