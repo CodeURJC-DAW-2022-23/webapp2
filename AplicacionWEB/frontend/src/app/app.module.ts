e00b8af1d88f2eb7be8e7bc896cc6559f4c2e9e3
DanielGallardo00 <59607988+DanielGallardo00@users.noreply.github.com>
2023-04-14 19:09:33 +0200

    cambios varios

--- AplicacionWEB/frontend/src/app/app.module.ts
+++ AplicacionWEB/frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { HeaderComponent } from './home/header/header.component';
+import { SearchComponent } from './home/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
