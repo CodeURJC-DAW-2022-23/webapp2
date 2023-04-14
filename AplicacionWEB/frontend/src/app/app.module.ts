412e93337cd15cc23971373c314bac223d3a5107
AndreaCardenalMillan <103501965+AndreaCardenalMillan@users.noreply.github.com>
2023-04-14 19:29:46 +0200

    modelos de comentarios y usuario

--- AplicacionWEB/frontend/src/app/app.module.ts
+++ AplicacionWEB/frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { HeaderComponent } from './home/header/header.component';
+import { CommentComponent } from './home/comment/comment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule
