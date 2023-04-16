836c235e8c2815b9afc06d4e93b1328bdd5abc11
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-04-16 21:09:42 +0200

    Revert "Formulario usuario componente (no funciona)"
    This reverts commit 0f259047f276dee066f3b9d62bc072e374a706f7.

--- AplicacionWEB/frontend/src/app/app.module.ts
+++ AplicacionWEB/frontend/src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './home/footer/footer.component';
 import { HeaderComponent } from './home/header/header.component';
 import { SearchComponent } from './home/search/search.component';
-import { EditUserComponent } from './user/edit-user/edit-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SearchComponent,
-    EditUserComponent
+    SearchComponent
   ],
   imports: [
     BrowserModule
