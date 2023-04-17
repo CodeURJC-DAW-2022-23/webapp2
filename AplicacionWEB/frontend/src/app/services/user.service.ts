a7234a4bc05722b6c64c74f37f34d8b8a31e1bbe
DanielGallardo00 <59607988+DanielGallardo00@users.noreply.github.com>
2023-04-17 12:51:31 +0200

    rutas añadidas

--- AplicacionWEB/frontend/src/app/services/user.service.ts
+++ AplicacionWEB/frontend/src/app/services/user.service.ts
@@ -1,8 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { UserProfile } from '../models/user.rest.model';
+
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+const BASE_URL = '/api/users';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +16,18 @@ export class UserService {
   constructor(private http: HttpClient ) { }
 
   getMe(): Observable<UserProfile>{
-    return this.http.get().pipe()as Observable<UserProfile>;
+    return this.http.get(BASE_URL+'/me', { withCredentials: true }).pipe()as Observable<UserProfile>;
+  }
+
+  register(formData: FormData){
+    return this.http.post(BASE_URL + "/", formData).pipe(
+      map((response: any) => {
+        return response;
+      }),
+      catchError((error: any) => {
+        return throwError('Algo salio mal');
+      })
+    );
   }
 
 }
