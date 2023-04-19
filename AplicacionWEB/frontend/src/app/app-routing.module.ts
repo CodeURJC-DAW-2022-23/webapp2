ed8850c941dd069681096a2e58b6c7d388660f22
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-04-19 20:40:37 +0200

    children routing

--- AplicacionWEB/frontend/src/app/app-routing.module.ts
+++ AplicacionWEB/frontend/src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './home/header/header.component';
+import { EditMyUserComponent } from './user/edit-my-user/edit-my-user.component';
+import { MyUserFavsComponent } from './user/my-user-favs/my-user-favs.component'
 
 const routes: Routes = [
-  {path:'', component: UserComponent}
+  {path:'', component: HeaderComponent},
+  {path:'myUser', component: UserComponent,
+  children: [
+    { path: '', component: EditMyUserComponent }, // url: about/
+    { path: 'favorites', component: MyUserFavsComponent } // url: about/item
+  ]
+  }
 ]
 
 @NgModule({
