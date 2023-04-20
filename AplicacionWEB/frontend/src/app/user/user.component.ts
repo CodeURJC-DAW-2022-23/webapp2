f3efdbd569d43b851fdced1b6d2bc46a478f8ea0
Danielus99 <102818915+Danielus99@users.noreply.github.com>
2023-04-20 23:41:26 +0200

    conectando myUser a servicio (sin exito)

--- AplicacionWEB/frontend/src/app/user/user.component.ts
+++ AplicacionWEB/frontend/src/app/user/user.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
+import { UserProfile } from '../models/user.rest.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit  {
+  u?:UserProfile;
   faBars = faBars;
   faUser = faUser;
-  username = "Hola"
+  username?:string;
   showme:boolean=true;
+
+  constructor(private userService:UserService){
+    
+  }
+
+  ngOnInit() {
+    this.loadUser();
+  }
+
+  loadUser(){
+    this.userService.getMe().subscribe(
+      response=>{
+        console.log(response);
+        this.u = response;
+        console.log(this.u);
+        this.username=this.u.user.username;
+      }
+    );
+    //this.username = this.u;
+  }
+
   toogleSidebar(){
     this.showme = !this.showme
   }
