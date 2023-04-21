519bac3102b8346b0565852ec5da59287181261f
AndreaCardenalMillan <103501965+AndreaCardenalMillan@users.noreply.github.com>
2023-04-21 19:10:06 +0200

    import event model

--- AplicacionWEB/frontend/src/app/admin/events/events.component.ts
+++ AplicacionWEB/frontend/src/app/admin/events/events.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Event } from '';
 
 @Component({
   selector: 'app-events',
---
+++ AplicacionWEB/frontend/src/app/models/event.model.ts
@@ -0,0 +1,15 @@
+export interface Event{
+    id:Number;
+    name:String;
+    date:String;
+    month:String;
+    description:String;
+    location:String;
+    asociation:String;
+    campus:string;
+    credits:Boolean;
+    booking:Boolean;
+    startTime:String;
+    endTime:String;
+    duration:String;
+}
