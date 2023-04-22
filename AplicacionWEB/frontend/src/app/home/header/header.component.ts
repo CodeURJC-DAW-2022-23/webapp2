bc8bfbabc37928c500bbe717b8daeac12b60d3c3
DanielGallardo00 <59607988+DanielGallardo00@users.noreply.github.com>
2023-04-22 13:36:45 +0200

    cambios nav

--- AplicacionWEB/frontend/src/app/home/header/header.component.ts
+++ AplicacionWEB/frontend/src/app/home/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  currentUser:User;
 
+  constructor(private userService:UserService){}
+
+  ngOnInit(): void {
+    this.userService.getMe().subscribe(
+      response=>{
+        this.currentUser = response;
+      } 
+    );
+  }
 }
